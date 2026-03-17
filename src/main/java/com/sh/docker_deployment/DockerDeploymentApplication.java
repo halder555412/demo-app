d8ca3c2ad2feab4af53705b4058a877a1ff7865d
Sajal <sajal18041998@gmail.com>
2026-03-17 22:56:52 +0530

    first commit

---
+++ src/main/java/com/sh/docker_deployment/DockerDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.sh.docker_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sh/docker_deployment/controller/Print.java
@@ -0,0 +1,15 @@
+package com.sh.docker_deployment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Print {
+
+    @GetMapping("/print")
+    public String print(){
+        return "This is test for deployment for Kubernates";
+    }
+}
